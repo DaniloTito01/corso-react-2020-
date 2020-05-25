d4174eb7c5a957d48ad577abb3e96ef676b9a365
Danilo <danilo.tito@conninf.com.br>
2020-05-25 19:20:56 -0300

    criação do  projeto cadastro de produtos

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function App(props) {
+
+  const modificaNome = event => {
+   console.log(event.target.value)
+  }
+
+  const criaComboBox = () => {
+    const opcoes = ["Danilo", "Tito"]
+    const comboxBoxOpcoes = opcoes.map(opcao => <option>{opcao}</option>)
+    return (
+      <select>
+        {comboxBoxOpcoes}
+      </select>
+    )
+  }
+
+
+
+const MeuComboBox = () => criaComboBox()
+return (
+  <>
+    <input className="text-centralizado" type="text" value={props.nome} onChange={modificaNome}></input>
+    <h1>Ola {props.nome} sua idade é {props.idade}</h1>
+     <MeuComboBox />
+
+  </>
+)
+
 }
 
+
 export default App;
+
+
